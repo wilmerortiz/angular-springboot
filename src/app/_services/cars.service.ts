e9ddb00328b89189e3ced635b92c0eb2637d13c1
alansastre <alan@alansastre.co>
2021-08-02 11:42:40 +0200

    heroku url

--- src/app/_services/cars.service.ts
+++ src/app/_services/cars.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 // const API_URL = 'http://localhost:8080/api/';
-const API_URL = 'https://alansastre-springboot-jwt.herokuapp.com/api/';
+const API_URL = 'https://alansastre-springboot-psql.herokuapp.com/api/';
 
 @Injectable({
   providedIn: 'root'
--- src/app/_services/hello.service.ts
+++ src/app/_services/hello.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-const API_URL = 'https://alansastre-springboot-jwt.herokuapp.com/api/';
+const API_URL = 'https://alansastre-springboot-psql.herokuapp.com/api/';
+//const API_URL = 'http://localhost:8080/api/';
 
 @Injectable({
   providedIn: 'root'
