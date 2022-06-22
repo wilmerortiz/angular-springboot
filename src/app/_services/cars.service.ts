59627ff5ae4883d271cb06a522be484059993ac3
wilmerortiz <jhak_dy@hotmail.com>
2022-06-22 01:28:59 -0500

    Cambio de nombre de proyecto

--- src/app/_services/cars.service.ts
+++ src/app/_services/cars.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 // const API_URL = 'http://localhost:8080/api/';
-const API_URL = 'https://alansastre-springboot-psql.herokuapp.com/api/';
+const API_URL = 'https://obspring-security-jwt.herokuapp.com/api/';
 
 @Injectable({
   providedIn: 'root'
--- src/app/_services/hello.service.ts
+++ src/app/_services/hello.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-const API_URL = 'https://alansastre-springboot-psql.herokuapp.com/api/';
+const API_URL = 'https://obspring-security-jwt.herokuapp.com/api/';
 //const API_URL = 'http://localhost:8080/api/';
 
 @Injectable({
